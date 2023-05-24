cfaf51e61a729fbb975bad3ede15905b86ac846d
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2023-05-24 10:06:35 -0300

    Func btnCripto & btnCopiar

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -12,3 +12,28 @@ function btnEncriptar() {
         textArea.value = "";
     }
 }
+
+function btnDecriptar() {
+    const textoEncriptado = decriptar(textArea.value);
+    if (textoEncriptado.length < 1) {
+        alert("Digite o texto a ser descriptografado");
+    } else {
+        textoSaida.value = textoEncriptado;
+        mostraMensagem();
+        textArea.value = "";
+    }
+}
+
+
+function btnCopiar() {
+    const textoCopiado = document.getElementById("texto-saida");
+    if (textoCopiado.value == "") {
+        alert("Não há mensagem a ser copiada");
+    } else {
+        textoCopiado.select();
+        document.execCommand("copy");
+        alert("Texto copiado para a área de transferência");
+        ocultaMensagem();
+        textoSaida.value = "";
+    }
+}
